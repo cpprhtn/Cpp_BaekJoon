f2477ca4a0ba39f8ad4ed13a401d1727469dc8b8
Lee_JunWon <cpprhtn@naver.com>
2020-07-01 21:10:05 +0900

    7/1

---
+++ 1371.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cctype>
+#include <algorithm>
+int main()
+{
+	char ch;
+	int arr[26]={}, cnt=0;
+	while(scanf("%c", &ch)!=EOF)
+	{
+		if(islower(ch))
+			cnt=std::max(cnt, ++arr[ch-'a']);
+	}
+
+	for(int i=0; i<26; i++)
+	{
+		if(arr[i]==cnt)
+			printf("%c", i);
+	}
+}
