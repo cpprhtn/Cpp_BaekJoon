50bad88742a2f2df4da6c4fb282537e23cee7f07
Lee_JunWon <cpprhtn@naver.com>
2020-07-30 23:47:52 +0900

    7/30

--- 1463.cpp
+++ 1463.cpp
@@ -37,3 +37,23 @@ int main()
 
 	printf("%d", count);
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[1000001];
+bool v[1000001];
+int n;
+int solve(int x)
+{
+    if(x==n) return 0;
+    if(v[x]) return dp[x];
+    if(x>n) return 1e9;
+    v[x]=true;
+    return dp[x] = 1+min({solve(x+1), solve(x*2), solve(x*3)});
+}
+int main()
+{
+    cin>>n;
+    cout<<solve(1);
+}
