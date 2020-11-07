fc5299951184e411257a310dcaad1f7efbb83000
Lee_JunWon <cpprhtn@naver.com>
2020-11-07 20:22:47 +0900

    11/7

---
+++ 1929.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int main()
+{
+    bool arr[100000] = {0};
+    int m, n;
+    scanf("%d %d", &m, &n);
+    for (int i = 2; i <= n; i++)
+    {
+        if (!arr[i])
+        {
+            if (i >= m)
+                printf("%d\n" , i)
+                
+            int temp = i;
+			while ((temp+=i) <= n)
+				arr[temp] = true;
+        }
+    }
+}
