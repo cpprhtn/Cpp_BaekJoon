21fda39d369f18a71b607d42cce76b8e4f8b11a0
Lee_JunWon <cpprhtn@naver.com>
2020-11-07 20:24:41 +0900

    11/7

--- 1929.cpp
+++ 1929.cpp
@@ -1,20 +1,20 @@
-#include <cstdio>
+#include<cstdio>
 
 int main()
 {
-    bool arr[100000] = {0};
-    int m, n;
-    scanf("%d %d", &m, &n);
-    for (int i = 2; i <= n; i++)
-    {
-        if (!arr[i])
-        {
-            if (i >= m)
-                printf("%d\n" , i)
-                
-            int temp = i;
+	bool arr[1000001] = {};
+	int m, n;
+	scanf("%d %d", &m, &n);
+	for (int i = 2; i <= n; i++)
+	{
+		if (!arr[i])
+		{
+			if (i >= m)
+				printf("%d\n", i);
+
+			int temp = i;
 			while ((temp+=i) <= n)
 				arr[temp] = true;
-        }
-    }
+		}
+	}
 }
