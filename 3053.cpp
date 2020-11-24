2adbeb0ed9ca78ad16f3a916d99ba7899bf251de
Lee_JunWon <cpprhtn@naver.com>
2020-11-25 01:22:12 +0900

    11/25

---
+++ 3053.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double PI=3.14159265358979323846;
+int main()
+{
+	long double r;
+	cin>>r;
+	cout.precision(20);
+	cout<<r*r*PI<<"\n"<<r*r*2;
+}
